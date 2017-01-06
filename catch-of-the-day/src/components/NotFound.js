9e730306e59aedf97cf19a009f54785ba00c1b51
vivek <vhprasad23@yahoo.com>
2017-01-05 21:08:36 -0500

    React router

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class NotFound extends React.Component{
+	render(){
+		return (
+			<h2>Not found!</h2>
+		)
+	}
+
+}
+
+
+export default NotFound;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,25 @@
 // let's go!
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
 import StorePicker from './components/StorePicker';
 import App from './components/App';
+import NotFound from './components/NotFound';
 import './css/style.css';
 
 
-render(<App/>, document.querySelector('#main'))
+const Root = () => {
+	return(
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker}/>
+				<Match pattern="/store/:storeId" component={App}/>
+				<Miss component={NotFound}/>
+			</div>
+		</BrowserRouter>
+
+	)
+	
+}
+
+render(<Root/>, document.querySelector('#main'))
